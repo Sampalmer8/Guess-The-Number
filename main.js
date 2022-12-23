496d99867b69708db5152a94cba479005487312b
Sampalmer8 <sampalmerg@gmail.com>
2022-12-23 10:29:30 -0700

    done

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-let randNum = (Math.random() * 100).toFixed(0);
+let randNum = +(Math.random() * 100).toFixed(0);
 let guesses = 0;
+const lower = "The number is lower than your guess";
+const higher = "The number is higher than your guess";
+let info = document.getElementById("info");
 
 document.getElementById("submit").addEventListener("click", numGuessed);
 document.getElementById("restart").addEventListener("click", restart);
 
 function numGuessed() {
   let guess = document.getElementById("guess").value;
-
   if (guess === randNum) {
     document.getElementById("output").innerHTML = "YOU WIN";
     guesses++;
@@ -14,11 +16,13 @@ function numGuessed() {
   } else if (guess > randNum) {
     document.getElementById("output").innerHTML =
       "The number is lower than your guess";
+    document.getElementById("info").innerHTML += `<li>${lower}</li>`;
     guesses++;
     setTimeout(clearOutput, 2000);
   } else {
     document.getElementById("output").innerHTML =
       "The number is higher than your guess";
+    document.getElementById("info").innerHTML += `<li>${higher}</li>`;
     guesses++;
     setTimeout(clearOutput, 2000);
   }
@@ -26,6 +30,7 @@ function numGuessed() {
   if (guesses === 7 && guess !== randNum) {
     setTimeout(lost, 2500);
   }
+  document.getElementById("amount").innerHTML = guesses;
 }
 
 function clearOutput() {
@@ -37,6 +42,10 @@ function restart() {
   guesses = 0;
   document.getElementById("output").innerHTML = "";
   document.getElementById("guess").value = "";
+  document.getElementById("amount").innerHTML = "";
+  while (info.hasChildNodes()) {
+    info.removeChild(info.firstChild);
+  }
 }
 
 function lost() {
